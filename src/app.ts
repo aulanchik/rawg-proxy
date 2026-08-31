e121399ae0539014720e5554b1f79bacd1214171
Artyom Ulanchik <45861755+aulanchik@users.noreply.github.com>
2026-08-31 21:26:29 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import express from 'express'
 import cors from 'cors'
-// import morgan from 'morgan'
+import morgan from 'morgan'
 import rawgRoutes from '@/routes/rawgRoutes'
 import { errorHandler } from '@/middleware/errorHandler'
 
 const app = express()
 
 app.use(cors())
-// app.use(morgan('dev'))
+app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
